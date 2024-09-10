23043b47c1b7d8e8a68d2d915fd0cf8165e2ad7c
Klapptnot Z3 <klapptnot@gmail.com>
2024-09-10 14:50:54 -0400

    add install script/config

---
+++ .conf.sh
@@ -0,0 +1,95 @@
+#! /bin/env bash
+# shellcheck disable=SC2034
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  # Break execution
+  printf "[\x1b[38;05;160m*\x1b[00m] This script is not made to run as a normal script\n"
+  exit 1
+fi
+
+pacman_pkgs=(
+  base
+  base-devel
+  bat
+  blueman
+  bluez-utils
+  brightnessctl
+  cliphist
+  efibootmgr
+  fastfetch
+  fd
+  flatpak
+  fzf
+  git
+  github-cli
+  grim
+  htop
+  hypridle
+  hyprland
+  hyprlock
+  hyprpaper
+  jq
+  kitty
+  loupe
+  neovim
+  nodejs
+  npm
+  ntfs-3g
+  nushell
+  papirus-icon-theme
+  python-pip
+  python-pipx
+  rofi-wayland
+  slurp
+  swaync
+  thunar
+  ttf-cascadia-code-nerd
+  tumbler
+  vivid
+  vlc
+  waybar
+  wget
+  yad
+  zip
+)
+
+yay_pkgs=(
+  carapace-bin
+  catppuccin-gtk-theme-mocha
+  hyprpicker
+  wlogout
+)
+
+nfolders=(
+  "${HOME}/.cache/hyprland"
+  "${HOME}/.cache/carapace"
+)
+
+function post_install {
+  CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' carapace _carapace nushell > ~/.cache/carapace/init.nu
+  bat cache --build &>/dev/null
+}
+
+function gen_ignore_list {
+  ignore=(
+    "dotf"
+    ".conf.sh"
+    "misc*"
+    ".git*"
+    ".config/nushell/history*"
+  )
+
+  [ -n "${TERMUX_VERSION@A}" ]    || ignore+=(".termux*")
+  # Desktop environment
+  command -v Hyprland > /dev/null || ignore+=(".config/hypr*")
+  command -v rofi > /dev/null     || ignore+=(".config/rofi*")
+  command -v swaync > /dev/null   || ignore+=(".config/swaync*")
+  command -v waybar > /dev/null   || ignore+=(".config/waybar*")
+  command -v wlogout > /dev/null  || ignore+=(".config/wlogout*")
+  # Shell
+  command -v kitty > /dev/null || ignore+=(".config/kitty*")
+  command -v nu > /dev/null    || ignore+=(".config/nushell*")
+  command -v fish > /dev/null  || ignore+=(".config/fish*")
+  command -v zsh > /dev/null   || ignore+=(".zshrc")
+  command -v bat > /dev/null   || ignore+=(".config/bat*")
+}
