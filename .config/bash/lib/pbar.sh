7d2658e91c2c6c9e608d3bbcacaba0b2a70cf3ef
Klapptnot Z3 <klapptnot@gmail.com>
2024-10-04 23:24:22 -0400

    fixes

--- .config/bash/lib/pbar.sh
+++ .config/bash/lib/pbar.sh
@@ -1,34 +1,23 @@
 #! /bin/env bash
-# shellcheck disable=SC2155 # Masking allowed ($?)
 
 # Function to display a nice functional progress bar
-function pb.do() {
+function pbar() {
   [ "${#}" -eq 0 ] && return
   local LEFT=false
   local CENTERED=false
-  local REMOVE_PBAR=true
+  local REMOVE_BAR=true
   local LINEBREAK=false
-  local UWIDTH=true
   local CHAR="•" # █
   local TERM_WIDTH=""
   local HINT=""
   while [ "${#}" -gt 0 ]; do
     case "${1}" in
-      -w | --width)
-        local WIDTH=${2}
-        shift 2
-        ;;
       -h | --hint)
         HINT=${2}
         shift 2
         ;;
       -e | --char)
         [ "${#2}" -eq 1 ] && CHAR="${2}"
-        shift 2
-        ;;
-      --term-width)
-        TERM_WIDTH="${2}"
-        UWIDTH=false
         shift 2
         ;;
       -l | --left)
@@ -40,7 +29,7 @@ function pb.do() {
         shift 1
         ;;
       -k | --keep)
-        REMOVE_PBAR=false
+        REMOVE_BAR=false
         shift 1
         ;;
       -n | --linebreak)
@@ -53,120 +42,99 @@ function pb.do() {
     esac
   done
 
-  local BAR_SIZES=(200 100 50 20 10)
-  [ -n "${WIDTH}" ] && {
-    # shellcheck disable=SC2076
-    if ! [[ " ${BAR_SIZES[*]} " =~ " ${WIDTH} " ]] || [ "$(((${#HINT} + WIDTH) + 7))" -gt "${TERM_WIDTH}" ]; then
-      unset WIDTH
-    fi
-  }
   [ -z "${TERM_WIDTH}" ] && TERM_WIDTH=$(tput cols)
+  local BAR_SIZES=(200 100 50 20 10)
 
-  ${UWIDTH} && [ -z "${WIDTH}" ] && {
-    for w in "${BAR_SIZES[@]}"; do
-      [ "$(((${#HINT} + w) + 7))" -le "${TERM_WIDTH}" ] && {
-        WIDTH="${w}"
-        break
-      }
-    done
-  }
+  local WIDTH
+  for w in "${BAR_SIZES[@]}"; do
+    (((${#HINT} + w) + 7 <= TERM_WIDTH)) && {
+      WIDTH="${w}"
+      break
+    }
+  done
 
-  # if [[ -p /dev/stdin ]]; then
-  #   # STDIN="$(cat -)"
-  #   read -r STDIN
-  # fi
-
-  local PROGRESS_OLD=0
-  local PBAR_STR=""
-  local LAST_DRAW=""
-
-  # [ "$(((${#HINT} + WIDTH) + 7))" -gt "${TERM_WIDTH}" ] && {/**/}
+  local LAST_STEP=0
+  local BAR_PROGRESS_CHARS=""
+  local LAST_LINE=""
+  local CURRENT_STEP=""
 
   while true; do
     # Check if data is available on stdin (non-blocking)
-    read -r -t 0.1 PROGRESS || continue
-    if [ -z "${PROGRESS}" ] || [[ "${PROGRESS}" != "[::pb.draw::] "* ]]; then
-      printf '\x1b[0K\x1b[0G%s\n%s' "${PROGRESS}" "${LAST_DRAW}" # Repeat logs
+    read -r -t 0.1 CURRENT_STEP || continue
+    if [ -z "${CURRENT_STEP}" ] || [[ "${CURRENT_STEP}" != "[::pb.draw::] "* ]]; then
+      printf '\x1b[0K\x1b[0G%s\n%s' "${CURRENT_STEP}" "${LAST_LINE}" # Repeat logs
       continue
     fi
-    PROGRESS="${PROGRESS:14:${#PROGRESS}}"
-    [ "${PROGRESS}" -le "${PROGRESS_OLD}" ] && continue
+
+    CURRENT_STEP="${CURRENT_STEP:14:${#CURRENT_STEP}}"
+    ((CURRENT_STEP <= LAST_STEP)) && continue
+    TERM_WIDTH=$(tput cols)
 
     # Enable auto-resizing when window resize occurs
-    for W in "${BAR_SIZES[@]}"; do
-      if [ "$(((${#HINT} + W) + 7))" -le "${TERM_WIDTH}" ]; then
-        WIDTH="${W}"
-        if [ "${LAST_DRAW}" != "${LAST_DRAW}" ]; then
-          # Set to 0 to redraw
-          PROGRESS_OLD=0
-          PBAR_STR=""
-        fi
-        LAST_DRAW="${W}"
+    for w in "${BAR_SIZES[@]}"; do
+      if (((${#HINT} + w) + 7 <= TERM_WIDTH)); then
+        WIDTH="${w}"
+        # Set to 0 to redraw
+        LAST_STEP=0
+        BAR_PROGRESS_CHARS=""
+        LAST_LINE="${w}"
         break
       fi
     done
 
-    if [ "${PROGRESS_OLD}" -eq 0 ]; then
-      # shellcheck disable=SC2183
-      if ${CENTERED}; then
-        local margin=$(((TERM_WIDTH - (WIDTH + 7)) / 2))
-        if [ "$((margin % 2))" -ne 0 ]; then
-          margin=$((margin - 1))
-        fi
-        [ -n "${HINT}" ] && {
-          HINT=$(printf "%s%*s" "${HINT}" $((margin - ${#HINT})))
-        } || HINT=$(printf "%*s" "${margin}")
-      elif [ -n "${HINT}" ]; then
-        [ "$(((${#HINT} + WIDTH) + 7))" -gt "${TERM_WIDTH}" ] && HINT="| "
-      fi
+    # shellcheck disable=SC2183
+    if ${CENTERED}; then
+      local margin=$(((TERM_WIDTH - (WIDTH + 7)) / 2))
+      (((margin % 2) != 0)) && margin=$((margin - 1))
+
+      printf -v SHOW_HINT "%s%*s" "${HINT}" $((margin - ${#HINT}))
+
+    elif [ -n "${HINT}" ]; then
+      ((((${#HINT} + WIDTH) + 7) > TERM_WIDTH)) && HINT="| "
     fi
 
-    local CHR_PL=""
-    local PAD_SPC=0
-    local PROGRESS_NUM=${PROGRESS}
+    local CHR_PL="${CHAR}"
+    local PAD_SPC=${WIDTH}
+    local DISPLAY_STEP_NUM=${CURRENT_STEP}
+    local AUGME=1
     case ${WIDTH} in
       200)
         CHR_PL="${CHAR}${CHAR}"
-        ${LEFT} && PROGRESS_NUM=$(((WIDTH / 2) - PROGRESS))
-        PAD_SPC=$((WIDTH - (PROGRESS * 2)))
-                                             ;;
+        ${LEFT} && DISPLAY_STEP_NUM=$(((WIDTH / 2) - CURRENT_STEP))
+        ;;
       100)
-        CHR_PL="${CHAR}"
-        ${LEFT} && PROGRESS_NUM=$((WIDTH - PROGRESS))
-        PAD_SPC=$((WIDTH - PROGRESS))
-                                       ;;
+        ${LEFT} && DISPLAY_STEP_NUM=$((WIDTH - CURRENT_STEP))
+        ;;
       50)
-        CHR_PL="${CHAR}"
-        ${LEFT} && PROGRESS_NUM=$(((WIDTH * 2) - PROGRESS))
-        PAD_SPC=$((WIDTH - (PROGRESS / 2)))
-                                             ;;
+        AUGME=2
+        ${LEFT} && DISPLAY_STEP_NUM=$(((WIDTH * 2) - CURRENT_STEP))
+        ;;
       20)
-        CHR_PL="${CHAR}"
-        ${LEFT} && PROGRESS_NUM=$(((WIDTH * 5) - PROGRESS))
-        PAD_SPC=$((WIDTH - (PROGRESS / 5)))
-                                             ;;
+        AUGME=5
+        ${LEFT} && DISPLAY_STEP_NUM=$(((WIDTH * 5) - CURRENT_STEP))
+        ;;
       10)
-        CHR_PL="${CHAR}"
-        ${LEFT} && PROGRESS_NUM=$(((WIDTH * 10) - PROGRESS))
-        PAD_SPC=$((WIDTH - (PROGRESS / 10)))
-                                              ;;
+        AUGME=10
+        ${LEFT} && DISPLAY_STEP_NUM=$(((WIDTH * 10) - CURRENT_STEP))
+        ;;
     esac
 
-    for ((i = PROGRESS_OLD; i < PROGRESS; i++)); do
-      local rnum=$(od -An -N1 -tu1 /dev/urandom)
-      PBAR_STR+="\x1b[38;5;${rnum#*\ }m${CHR_PL}\x1b[0m"
+    for ((i = LAST_STEP; i < CURRENT_STEP; i += AUGME)); do
+      ((PAD_SPC -= 1))
+      read -r rnum < <(od -An -N1 -tu1 /dev/urandom)
+      BAR_PROGRESS_CHARS+="\x1b[38;5;${rnum}m${CHR_PL}\x1b[0m"
+      ((WIDTH == 200)) && ((PAD_SPC -= 1))
     done
-    PROGRESS_OLD=${PROGRESS}
 
-    LAST_DRAW=$(printf "\x1b[0K%s[%b%${PAD_SPC}s] %4s\x1b[0G" "${HINT}" "${PBAR_STR}" "" "${PROGRESS_NUM}%")
-    printf '%s' "${LAST_DRAW}"
+    LAST_STEP=${CURRENT_STEP}
 
-    ${LEFT} && [[ ${PROGRESS} -gt 1 ]] && continue
-    ! ${LEFT} && [[ ${PROGRESS} -lt 100 ]] && continue
+    printf -v LAST_LINE "\x1b[0K%s[%b%${PAD_SPC}s] %4s\x1b[0G" "${SHOW_HINT}" "${BAR_PROGRESS_CHARS}" "" "${DISPLAY_STEP_NUM}%"
+    printf '%s' "${LAST_LINE}"
 
+    ((CURRENT_STEP < 100)) && continue
     break
   done
-  ${REMOVE_PBAR} && printf '\x1b[0K\x1b[0G'
+  ${REMOVE_BAR} && printf '\x1b[0K\x1b[0G'
   ${LINEBREAK} && printf "\n"
-  echo >&2
+  return 0
 }
